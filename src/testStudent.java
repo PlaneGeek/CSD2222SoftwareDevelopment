1fdaa1342a0cbaad8644c1fa5b0fdf7592123f67
Nebbyyy <b.allington@hotmail.co.uk>
2014-11-03 13:32:04 +0000

    testStudent.java FML

---
+++ src/testStudent.java
@@ -0,0 +1,10 @@
+ 
+public class testStudent {
+    public static void main (String[] args){
+         
+        Student student1 = new Student ("John", 19, 'M');
+        student1.setProgramme("Computing");
+        student1.getProgramme();
+        student1.displayStudentDetails();
+    }
+}
